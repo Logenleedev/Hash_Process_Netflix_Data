ce3fd2bb35414a6a973d1f999dde6f48c8d5486c
Logen <47022821+Logenleedev@users.noreply.github.com>
2022-05-11 15:17:26 -0500

    display CSV file done

--- client/js/client.js
+++ client/js/client.js
@@ -1,5 +1,45 @@
-console.log('Hello! I am client.js inside client/js/...');
-console.log('...This comment is on line 2 of client.js');
+const fileInput = document.getElementById('file')
+const submitFile = document.getElementById('submitFile')
+let file = null
 
-// Insert the 'Hey there!' text on the template page
-document.getElementById('greeting').innerText = 'Hey there!';
+fileInput.addEventListener('change', function () {
+  file = this.files[0]
+})
+
+submitFile.addEventListener('click', function () {
+  if (!file || file.type !== 'text/csv') {
+    alert('Please choose a CSV file')
+    return
+  }
+
+  const formData = new FormData()
+  formData.append('file', file)
+
+  fetch('/read', {
+    method: 'POST',
+    body: formData
+  })
+  .then((response) => response.json())
+  .then(({ data }) => {
+    if (data.length) {
+      const columns = data[0]
+      const rows = data.splice(1).map((arr) => {
+        const obj = {}
+        columns.forEach((column, index) => {
+          obj[column] = arr[index]
+        })
+        return obj
+      })
+
+      console.log(rows, columns)
+      
+      const table = new Tabulator("#csvTable", {
+        height:"300px",
+        data: rows,
+        autoColumns: true
+      });
+    } else {
+      alert('The CSV is empty')
+    }
+  })
+  .catch((e) => alert(e.message))})
--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-var http = require('http');
-var path = require('path');
+const http = require('http');
+const path = require('path');
 
-var express = require('express');
-
+const express = require('express');
+const parse = require('csv-parse').parse
+const os = require('os')
+const multer  = require('multer')
+const upload = multer({ dest: os.tmpdir() })
+const fs = require('fs')
+const stringify = require('csv-stringify').stringify
+const bodyParser = require('body-parser')
 //
 // ## SimpleServer `SimpleServer(obj)`
 //
@@ -12,10 +18,28 @@ var express = require('express');
 var router = express();
 var server = http.createServer(router);
 
+const app = express();
+
+app.use(bodyParser.json())
 router.use(express.static(path.resolve(__dirname, 'client')));
 
 console.log('Booting up the server! Please wait until finished...')
+
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
   console.log("All ready! Server listening at", addr.address + ":" + addr.port);
 });
+
+router.post('/read', upload.single('file'), (req, res) => {
+  const file = req.file
+
+  const data = fs.readFileSync(file.path)
+  parse(data, (err, records) => {
+    if (err) {
+      console.error(err)
+      return res.status(400).json({success: false, message: 'An error occurred'})
+    }
+
+    return res.json({data: records})
+  })
+})
